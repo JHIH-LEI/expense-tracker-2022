1eef677a6d204561e601e9e89f74f8f96e1fa1c6
JHIH-LEI <vicky88888@gmail.com>
2022-05-08 21:55:33 +0800

    modify httpGoogleLogin httpGoogleLoginCB

--- functions/src/http/login/google.ts
+++ functions/src/http/login/google.ts
@@ -1,13 +1,7 @@
-// import { http } from "@google-cloud/functions-framework";
+import { http } from "@google-cloud/functions-framework";
 import { googleAuthURL } from "../../init";
-import express, { Response, Request } from "express";
-const http = express();
+import { Response, Request } from "express";
 
-export default http.get(
-  "/login/google",
-  async (req: Request, res: Response) => {
-    return res.redirect(googleAuthURL);
-  }
-);
-
-http.listen(3000, () => console.log(""));
+export default http("/login/google", async (req: Request, res: Response) => {
+  return res.redirect(googleAuthURL);
+});
--- functions/src/http/login/googleCallback.ts
+++ functions/src/http/login/googleCallback.ts
@@ -1,63 +1,63 @@
-// import { http } from "@google-cloud/functions-framework";
+import { http } from "@google-cloud/functions-framework";
 import { googleAuthClient, google, prisma } from "../../init";
 import { ErrorCode, ErrorCodeMapToStatus } from "../../types/error";
 import { hashSync } from "bcrypt";
-import express from "express";
-const http = express();
+import { Request, Response } from "express";
 
-export default http.use("/googleAuthCallback", async (req, res) => {
-  const { code } = req.query as { code: string };
-  const { tokens } = await googleAuthClient.getToken(code);
+export default http(
+  "/googleAuthCallback",
+  async (req: Request, res: Response) => {
+    const { code } = req.query as { code: string };
+    const { tokens } = await googleAuthClient.getToken(code);
 
-  googleAuthClient.credentials = tokens;
-  const oauth2 = google.oauth2("v2");
+    googleAuthClient.credentials = tokens;
+    const oauth2 = google.oauth2("v2");
 
-  const {
-    data: { email, id: google_open_id },
-  } = await oauth2.userinfo.v2.me
-    .get({ auth: googleAuthClient })
-    .catch((error) => {
+    const {
+      data: { email, id: google_open_id },
+    } = await oauth2.userinfo.v2.me
+      .get({ auth: googleAuthClient })
+      .catch((error) => {
+        throw new Error(
+          JSON.stringify({
+            code: ErrorCode.GOOGLE_OAUTH_ERROR,
+            status: ErrorCodeMapToStatus[ErrorCode.GOOGLE_OAUTH_ERROR],
+            message: "send code to exchange token have unexpected error",
+            error,
+          })
+        );
+      });
+
+    if (!email) {
       throw new Error(
         JSON.stringify({
-          code: ErrorCode.GOOGLE_OAUTH_ERROR,
-          status: ErrorCodeMapToStatus[ErrorCode.GOOGLE_OAUTH_ERROR],
+          code: ErrorCode.INVALID_EMAIL,
+          status: ErrorCodeMapToStatus[ErrorCode.INVALID_EMAIL],
           message: "send code to exchange token have unexpected error",
-          error,
         })
       );
-    });
+    }
 
-  if (!email) {
-    throw new Error(
-      JSON.stringify({
-        code: ErrorCode.INVALID_EMAIL,
-        status: ErrorCodeMapToStatus[ErrorCode.INVALID_EMAIL],
-        message: "send code to exchange token have unexpected error",
-      })
-    );
-  }
+    const isNewUser = !(await prisma.user.findUnique({ where: { email } }));
 
-  const isNewUser = !(await prisma.user.findUnique({ where: { email } }));
+    if (isNewUser) {
+      const randomPassword = (Math.random() + 1).toString(36).substring(2);
 
-  if (isNewUser) {
-    const randomPassword = (Math.random() + 1).toString(36).substring(2);
+      const password = hashSync(randomPassword, 10);
 
-    const password = hashSync(randomPassword, 10);
+      await prisma.user.create({
+        data: {
+          username: google_open_id || email.slice(0, 5),
+          email,
+          password,
+          icon: null,
+          refreshToken: tokens.refresh_token || null,
+        },
+      });
+    }
 
-    await prisma.user.create({
-      data: {
-        username: google_open_id || email.slice(0, 5),
-        email,
-        password,
-        icon: null,
-        refreshToken: tokens.refresh_token || null,
-      },
+    res.status(200).json({
+      token: tokens.access_token,
     });
   }
-
-  res.status(200).json({
-    token: tokens.access_token,
-  });
-});
-
-http.listen(3000, () => console.log(""));
+);
