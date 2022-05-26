3474f02b2e619b7e9c19fab4f58f94aacc8a52d1
JHIH-LEI <vicky88888@gmail.com>
2022-05-26 20:52:05 +0800

    feat: account level and group capacity type

--- functions/src/types/common.ts
+++ functions/src/types/common.ts
@@ -7,4 +7,12 @@ export enum DEFAULT_CATEGORY {
   "交通",
 }
 
+export enum ACCOUNT_LEVEL {
+  FREE = "FREE",
+}
+
+export const GROUP_CAPACITY = {
+  [ACCOUNT_LEVEL.FREE]: 10,
+};
+
 export type RequestWithJWT = Request & Record<"user", { user_id: number }>;
