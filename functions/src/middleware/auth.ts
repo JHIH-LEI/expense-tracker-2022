99c73eeacbf3673cae421b399b29c33137453fd3
JHIH-LEI <vicky88888@gmail.com>
2022-05-27 11:59:43 +0800

    modify auth middleware

--- functions/src/middleware/auth.ts
+++ functions/src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import { Request, NextFunction, Response } from "express";
 import "dotenv";
+import { RequestWithJWT } from "../types/common";
 
 export default async function authenticator(
   req: Request & { user: jwt.JwtPayload },
@@ -9,9 +10,19 @@ export default async function authenticator(
 ) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
+  const { user_id } = req.params;
+  const {
+    user: { user_id: authId },
+  } = req as RequestWithJWT;
 
   if (!token) {
     return res.sendStatus(401);
+  }
+
+  if (user_id && parseInt(user_id) !== authId) {
+    return res
+      .status(403)
+      .end(`user id:${user_id} not equal to login in user id :${authId}.`);
   }
 
   jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET!, (err, user) => {
